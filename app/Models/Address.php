af822ae6d557d08309717557113ee5414d693635
Victor Farias <vgfr456@gmail.com>
2024-06-27 21:47:47 -0300

    feat: add attributes and functions on Address model

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Address extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'user_id', 'street', 'city', 'state', 'country', 'postal_code'
+    ];
+
+    protected $table = 'addresses';
+    protected $keyType = 'string';
+    protected $primaryKey = 'address_id';
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
