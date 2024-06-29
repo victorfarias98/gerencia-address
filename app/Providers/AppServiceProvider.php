3da83b2eac49b0452a6ada5cd4a8523167e004f2
Victor Farias <vgfr456@gmail.com>
2024-06-29 13:35:51 -0300

    feat: add api routes on app.php and fix composer files and add interfaces on provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,14 @@
 
 namespace App\Providers;
 
+use App\Repositories\AddressRepository;
+use App\Repositories\Interfaces\AddressRepositoryInterface;
+use App\Repositories\Interfaces\UserRepositoryInterface;
+use App\Repositories\UserRepository;
+use App\Services\AddressService;
+use App\Services\Interfaces\AddressServiceInterface;
+use App\Services\Interfaces\UserServiceInterface;
+use App\Services\UserService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +19,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(UserServiceInterface::class, UserService::class);
+        
+        $this->app->bind(AddressRepositoryInterface::class, AddressRepository::class);
+        $this->app->bind(AddressServiceInterface::class, AddressService::class);
     }
 
     /**
